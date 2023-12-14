1a358d7be60450f1e2a1866ba1a872f2869ea2a4
Hamzah212 <133094712+Hamzah212@users.noreply.github.com>
2023-12-14 13:37:26 +0100

    Add files via upload

--- process.php
+++ process.php
@@ -1,73 +1,65 @@
+[12:31] Hamzah Iqbal
 <?php
-// Verbinding maken met de database (vervang met je eigen databasegegevens)
-$servername = "localhost";
-$username = "gebruikersnaam";
-$password = "wachtwoord";
-$dbname = "database";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Toevoegen van jongeren
-if (isset($_POST['toevoegen'])) {
-    $naam = $_POST['naam'];
-    $leeftijd = $_POST['leeftijd'];
-
-    $sql = "INSERT INTO jongeren (naam, leeftijd) VALUES ('$naam', '$leeftijd')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Jongere succesvol toegevoegd";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+include 'databaseconn.php';
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_person"])) {
+        $naam = $_POST["Naam"];
+        $leeftijd = $_POST["Achternaam"];
+        $achternaam = $_POST["Leeftijd"];
+        $activiteit = $_POST["Email"];
+        $begeleider = $_POST["Telefoonnummer"];
+        $stmt = $conn->prepare("INSERT INTO jongeren_activiteit (naam, achternaam, leeftijd, activiteit, begeleider) VALUES (:naam, :achternaam, :leeftijd, :activiteit, :begeleider)");
+        $stmt->execute([
+        ':naam' => $naam,
+        ':achternaam' => $achternaam,
+        ':leeftijd' => $leeftijd,
+        ':email' => $email,
+        ':telefoonnummer' => $telefoonnummer,
+        ]);
+        echo "Persoon succesvol toegevoegd.";
     }
-}
-
-// Wijzigen van jongeren
-if (isset($_POST['wijzigen'])) {
-    $id = $_POST['id_wijzigen'];
-    $nieuweNaam = $_POST['nieuwe_naam'];
-    $nieuweLeeftijd = $_POST['nieuwe_leeftijd'];
-
-    $sql = "UPDATE jongeren SET naam='$nieuweNaam', leeftijd='$nieuweLeeftijd' WHERE id=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Jongere succesvol gewijzigd";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    // Verwijder een persoon
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["delete_person"])) {
+        $id_to_delete = $_POST["id_to_delete"];
+ 
+        $stmt = $conn->prepare("DELETE FROM jongeren_activiteit WHERE id = :id");
+        $stmt->execute([':id' => $id_to_delete]);
+ 
+        echo "Persoon succesvol verwijderd.";
     }
+// Update een persoon
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["update_person"])) {
+    $id_to_update = $_POST["id_to_update"];
+    $naam = $_POST["naam"];
+    ':naam' => $naam,
+        ':achternaam' => $achternaam,
+        ':leeftijd' => $leeftijd,
+        ':email' => $email,
+        ':telefoonnummer' => $telefoonnummer,
+ 
+    $stmt = $conn->prepare("UPDATE jongeren_activiteit SET naam = :naam, achternaam = :achternaam, leeftijd = :leeftijd, activiteit = :activiteit, begeleider = :begeleider WHERE id = :id");
+ 
+    $stmt->execute([
+        ':naam' => $naam,
+        ':achternaam' => $achternaam,
+        ':leeftijd' => $leeftijd,
+        ':email' => $email,
+        ':telefoonnummer' => $telefoonnummer,
+    ]);
+ 
+    echo "Persoon succesvol bijgewerkt.";
 }
-
-// Verwijderen van jongeren
-if (isset($_POST['verwijderen'])) {
-    $id = $_POST['id_verwijderen'];
-
-    $sql = "DELETE FROM jongeren WHERE id=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Jongere succesvol verwijderd";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+?>
+<?php
+$servername = "Hamzah";
+$username = "root";
+$password = "";
+$dbname = "administratie";
+ 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-
-// Koppelen aan instituut
-if (isset($_POST['koppelen_instituut'])) {
-    $id = $_POST['id_koppelen'];
-    $instituut = $_POST['instituut'];
-
-    // Voeg code toe om de koppeling met het instituut te maken
-}
-
-// Koppelen aan activiteit
-if (isset($_POST['koppelen_activiteit'])) {
-    $id = $_POST['id_koppelen'];
-    $activiteit = $_POST['activiteit'];
-
-    // Voeg code toe om de koppeling met de activiteit te maken
-}
-
-$conn->close();
 ?>
